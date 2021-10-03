3e7e01fab2fddb3b28080baae9f5a69035be468a
TharindiHewawasam <malharit74@gmail.com>
2021-10-04 01:37:33 +0530

    merge added

--- app/src/main/java/com/example/car2go/ui/services/ServicesFragment.java
+++ app/src/main/java/com/example/car2go/ui/services/ServicesFragment.java
@@ -4,60 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-
-import android.content.Intent;
-
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.car2go.R;
 import com.example.car2go.databinding.FragmentServicesBinding;
 
 
 public class ServicesFragment extends Fragment {
-    private Button inquiry,rate_us,con_us;
 
-
-    private @NonNull FragmentServicesBinding binding;
+    private FragmentServicesBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
 
         binding = FragmentServicesBinding.inflate(inflater, container, false);
-        View root = inflater.inflate(R.layout.fragment_services, container, false);
-
-        inquiry = root.findViewById(R.id.inquiry);
-        inquiry.setOnClickListener(new View.OnClickListener(){
-            @Override
-                    public void onClick(View v){
-                            Intent intent = new Intent(getActivity(), AddInquiry.class);
-                            startActivity(intent);
-            }
-        });
-        rate_us = root.findViewById(R.id.rate_us);
-        rate_us.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(getActivity(), AddRating.class);
-                startActivity(intent);
-            }
-        });
-        con_us = root.findViewById(R.id.con_us);
-        con_us.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(getActivity(), ContactUs.class);
-                startActivity(intent);
-            }
-        });
-
+        View root = binding.getRoot();
 
         return root;
-
     }
 }
